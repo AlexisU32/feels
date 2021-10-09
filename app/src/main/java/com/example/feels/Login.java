25316d3cb7888726d2936d4558db118ed5bdecfc
alexis <uribea360@gmail.com>
2021-10-08 21:40:09 -0500

    Primer comit de prueba

---
+++ app/src/main/java/com/example/feels/Login.java
@@ -0,0 +1,37 @@
+package com.example.feels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Hacemos que esta vista se vea en toda la pantalla
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        // Verificar si el usuario está conectado a internet
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Toast.makeText(getApplicationContext(), "Está conectado " + networkInfo.getState(), Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "No está conectado ", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+
+
+    }
+}
