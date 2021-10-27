9441d6de3adf0d1fd5384a2c21e99cc0bdb7db1f
alexis <uribea360@gmail.com>
2021-10-26 23:29:37 -0500

    Se hacen arreglos al activity registro usuarios

---
+++ app/src/main/java/com/example/feels/Recuperar_password.java
@@ -0,0 +1,14 @@
+package com.example.feels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recuperar_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_password);
+    }
+}
--- app/src/main/java/com/example/feels/RegistrarUser.java
+++ app/src/main/java/com/example/feels/RegistrarUser.java
@@ -2,13 +2,40 @@ package com.example.feels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class RegistrarUser extends AppCompatActivity {
+
+    Button volver_al_login;
+    CheckBox terminos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_user);
+        volver_al_login = findViewById(R.id.btn_volver_registrar_user);
+        terminos = findViewById(R.id.check_terminos_condiciones);
+
+        /**
+         * @author Alexis Ruiz Uribe
+         * @date 26/10/2021
+         * @params
+         * @resumen Método para redireccionar al Login
+         */
+        volver_al_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+
+
+
+
+
     }
 }
